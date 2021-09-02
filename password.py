06de5a9cb33d7b7e8cea1c90a7ff05232ae045b5
Naomi Ch <naomi.chika.1@gmail.com>
2021-09-02 19:54:44 +0300

    create def init + save_user method

---
+++ password.py
@@ -0,0 +1,15 @@
+class User:
+   """
+   Class that generates new instance of user
+   """
+   user_list = []
+   def __init__(self,user_name,password):
+     self.user_name = user_name
+     self.password = password 
+
+
+   def save_user(self):
+     '''
+     save_user method saves new user objects to the user list
+     '''
+     User.user_list.append(self)
